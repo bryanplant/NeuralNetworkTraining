b1a5e8493afa6bf0f1b7ebc3a10d242e7ae30fe6
Bryan Plant <bryanplant@gmail.com>
2017-11-03 12:42:18 -0600

    added comments and simplified main method

--- src/network/GeneticAlgorithm.java
+++ src/network/GeneticAlgorithm.java
@@ -7,18 +7,19 @@ public class GeneticAlgorithm {
 	private static Random rand = new Random();
 
 	public static ArrayList<Network> createNextGeneration(ArrayList<Network> networks) {
-		ArrayList<Network> newGeneration = select(networks);
-		newGeneration = crossover(newGeneration);
-		newGeneration = mutate(newGeneration);
-		return newGeneration;
+		ArrayList<Network> newGeneration = select(networks);	//select parents
+		newGeneration = crossover(newGeneration);				//crossover parents
+		newGeneration = mutate(newGeneration);					//mutate offspring
+		return newGeneration;									//return offspring
 	}
 
 	private static ArrayList<Network> select(ArrayList<Network> networks) {
-		ArrayList<Network> parents = new ArrayList<Network>();
-		ArrayList<Network> pool = new ArrayList<Network>();
+		ArrayList<Network> parents = new ArrayList<Network>();	//list containing selected parents
+		ArrayList<Network> pool = new ArrayList<Network>();		//list representing the pool of individuals to pick from
 
 		// add each network to the gene pool a certain number of times corresponding to
 		// the fitness of the network
+		// networks with a higher fitness will be added to the pool more times
 		for (int i = 0; i < networks.size(); i++) {
 			for (int j = 0; j < (int) networks.get(i).getFitness(); j++) {
 				pool.add(networks.get(i));
@@ -35,38 +36,43 @@ public class GeneticAlgorithm {
 	}
 
 	private static ArrayList<Network> crossover(ArrayList<Network> parents){
-		ArrayList<Network> offspring = new ArrayList<Network>();
-		while(!parents.isEmpty()){
-			Network parent1 = parents.get(0);
-			Network parent2 = parents.get(rand.nextInt(parents.size()-1)+1);
-			ArrayList<ArrayList<Double>> parentGenes1 = parent1.getGenes();
-		    ArrayList<ArrayList<Double>> parentGenes2 = parent2.getGenes();
-		    ArrayList<ArrayList<Double>> offspringGenes1 = new ArrayList<>();
-		    ArrayList<ArrayList<Double>> offspringGenes2 = new ArrayList<>();
-		    for(int i = 0; i < parentGenes1.size(); i++){
+		ArrayList<Network> offspring = new ArrayList<Network>();	//result of crossover of parents
+		while(!parents.isEmpty()){	//each iteration two parents will be removed from parents list
+			Network parent1 = parents.get(0);	//get the first individual in list
+			Network parent2 = parents.get(rand.nextInt(parents.size()-1)+1);	//get a random individual from list that is not parent 1
+			ArrayList<ArrayList<Double>> parentGenes1 = parent1.getGenes();		//get parent 1 genes
+		    ArrayList<ArrayList<Double>> parentGenes2 = parent2.getGenes();		//get parent 2 genes
+		    ArrayList<ArrayList<Double>> offspringGenes1 = new ArrayList<>();	//initialize offspring 1 genes
+		    ArrayList<ArrayList<Double>> offspringGenes2 = new ArrayList<>();	//initialize offspring 2 genes
+		    for(int i = 0; i < parentGenes1.size(); i++){	//initialize the size of the outer offspring genes list
 		    	offspringGenes1.add(new ArrayList<Double>());
 		    	offspringGenes2.add(new ArrayList<Double>());
 		    }
-		    for (int i = 0; i < parentGenes1.size(); i++) {
-		    	for(int j = 0; j < parentGenes1.get(i).size(); j++) {
-		    		if(rand.nextInt(2) == 0){
-		    			offspringGenes1.get(i).add(parentGenes1.get(i).get(j));
-		    			offspringGenes2.get(i).add(parentGenes2.get(i).get(j));
+		    for (int i = 0; i < parentGenes1.size(); i++) {	//iterate through outer list
+		    	for(int j = 0; j < parentGenes1.get(i).size(); j++) {	//iterate through inner list
+		    		if(rand.nextInt(2) == 0){	//select either 0 or 1 randomly
+		    			offspringGenes1.get(i).add(parentGenes1.get(i).get(j)); //set gene i of offspring 1 to gene i of parent 1
+		    			offspringGenes2.get(i).add(parentGenes2.get(i).get(j)); //set gene i of offspring 2 to gene i of parent 2
 		    		}
 		    		else{
-		    			offspringGenes1.get(i).add(parentGenes2.get(i).get(j));
-		    			offspringGenes2.get(i).add(parentGenes1.get(i).get(j));
+		    			offspringGenes1.get(i).add(parentGenes2.get(i).get(j)); //set gene i of offspring 1 to gene i of parent 2
+		    			offspringGenes2.get(i).add(parentGenes1.get(i).get(j)); //set gene i of offspring 1 to gene i of parent 1
 		    		}
 		    	}
 		    }
+		    //set the structure of offspring network the same as parent network
 		    int numInputs = parent1.getNumInputs();
 		    int numHidLayers = parent1.getNumHidLayers();
 		    int numHidNodes = parent1.getNumHidNodes();
 		    int numOutputs = parent1.getNumOutputs();
 		    int actFunHidden = parent1.getActFunHidden();
 		    int actFunOutput = parent1.getActFunOutput();
+		    
+		    //add offspring to offspring list
 		    offspring.add(new Network(offspringGenes1, numInputs, numHidLayers, numHidNodes, numOutputs, actFunHidden, actFunOutput));
 		    offspring.add(new Network(offspringGenes2, numInputs, numHidLayers, numHidNodes, numOutputs, actFunHidden, actFunOutput));
+		    
+		    //remove the parents that were crossed over from the list of parents
 		    parents.remove(parent1);
 		    parents.remove(parent2);
 		}
@@ -75,17 +81,17 @@ public class GeneticAlgorithm {
 	}
 
 	private static ArrayList<Network> mutate(ArrayList<Network> networks) {
-		ArrayList<Network> mutated = new ArrayList<Network>();
-	    for(Network network : networks){
-	    	for(int i = 0; i < network.getGenes().size(); i++) {
-	    		for(int j = 0; j < network.getGenes().get(i).size(); j++){
-	    			if (rand.nextDouble() <= network.getMutationRate()) {
-	    				double gene = network.getGenes().get(i).get(j);
-	    				network.setGene(i, j, gene + (rand.nextDouble()-.5)/2);
+		ArrayList<Network> mutated = new ArrayList<Network>(); //list of mutated networks
+	    for(Network network : networks){	//iterate through all networks
+	    	for(int i = 0; i < network.getGenes().size(); i++) {	//iterate through rows of genes
+	    		for(int j = 0; j < network.getGenes().get(i).size(); j++){	//iterate through columns of genes
+	    			if (rand.nextDouble() <= network.getMutationRate()) {	//if a random number is less than or equal to the mutation rate then mutate
+	    				double gene = network.getGenes().get(i).get(j);	//get gene that is to be mutated
+	    				network.setGene(i, j, gene + (rand.nextDouble()-.5)/2); //add a random number between -.25 and .25 to the gene
 	    			}
 	    		}
 	    	}
-	      mutated.add(network);
+	      mutated.add(network);	//add mutated individual to list
 	    }
 	    return mutated;
 	}
--- src/network/Main.java
+++ src/network/Main.java
@@ -8,106 +8,50 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);																	//create scanner for input
-		boolean valid = true;																					//flag variable for correct user input
-		int input = 0, numInputs = 0, numDataPoints = 0;														//numInputs here is #of input nodes; input is user choice
-		Network network = null;
-
-		//opening menu - prompt user to create a new data set
-		do {
-			valid = true;												//assume input is valid
-			try {
-				System.out.println("Would you like to create a new data set?\n1. Yes\n2. No");
-				System.out.print(">");
-				input = in.nextInt();									//attempt to read integer input for choice
-			}
-			catch (Exception e) {										//if that fails
-				System.out.println("That is not an integer. Please enter an integer.\n");
-				valid = false;											//input is invalid, flag
-				in.nextLine();											//clear input buffer
-			}
-		} while (valid == false);										//repeat if bad choice
-
 		ArrayList<Sample> samples = new ArrayList<Sample>();			//create list of samples to use - data set essentially
+		int numInputs = 0;
+		int numDataPoints = 0;
 
-		//entering the dimensions - if the user chooses to create new data set
-		if (input == 1) {
-			do {
-				valid = true;
-				try {
-					System.out.println("Please enter the number of dimensions for the Rosenbrock function:");
-					numInputs = in.nextInt();
+		numDataPoints = 0;
+		try {
+			Scanner s = new Scanner(new File("function-finding.txt"));							//create a new scanner, checks lines of data in file
+			while (s.hasNextLine()) {												//loop while there is another line
+				String line = s.nextLine();											//grab the next line
+				ArrayList<Double> inputs = new ArrayList<Double>();					//create an arraylist for the inputs
+				int counter = 0;													//counter to determine size of input array
+				Scanner lineScan = new Scanner(line);								//create new scanner that checks individual pieces of a line
+				lineScan.useDelimiter(",");										//separates tokens with a comma, space (as they were inputted that way)
+				while (lineScan.hasNext()) {										//loop while there are still tokens to be read
+					counter++;														//update counter (1 more input)
+					double element = Double.parseDouble(lineScan.next());
+					inputs.add(element);				//parse the token to be a double and add to the input arraylist
+				}														//update counter to reflect input size (total - 1, since last token is output
+				counter--;
+				double[] passIn = new double[counter];								//this is the array that will be passed to sample class
+				for (int i = 0; i < counter; i++) {
+					passIn[i] = inputs.get(i);										//initialize the input array
 				}
-				catch (Exception e) {
-					System.out.println("That is not an integer. Please enter an integer.\n");
-					valid = false;
-					in.nextLine();
-				}
-			} while (valid == false);
-
-			//enter data points (or inputs)
-			do {
-				valid = true;
-				try {
-					System.out.println("Please enter the number of data points you want:");
-					numDataPoints = in.nextInt();
-				}
-				catch (Exception e) {
-					System.out.println("That is not an integer. Please enter an integer.\n");
-					valid = false;
-					in.nextLine();
-				}
-			} while (valid == false);
-
-			RosenbrockGenerator gen = new RosenbrockGenerator();
-			samples = gen.generate(numInputs, numDataPoints);
+				samples.add(new Sample(passIn, inputs.get(counter)));				//create new sample with input array, output, and add that sample to the list of samples
+				numInputs = passIn.length;
+				numDataPoints ++;
+				lineScan.close();
+			}
+			s.close();
+			System.out.println("Number of Inputs: " + numInputs);
+			System.out.println("Number of Data Points: " + numDataPoints);
 		}
-
-		//user chooses not to create data file - assumes one exists already
-		else {
-			numDataPoints = 0;
-			try {
-				Scanner s = new Scanner(new File("function-finding.txt"));							//create a new scanner, checks lines of data in file
-				while (s.hasNextLine()) {												//loop while there is another line
-					String line = s.nextLine();											//grab the next line
-					ArrayList<Double> inputs = new ArrayList<Double>();					//create an arraylist for the inputs
-					int counter = 0;													//counter to determine size of input array
-					Scanner lineScan = new Scanner(line);								//create new scanner that checks individual pieces of a line
-					lineScan.useDelimiter(",");										//separates tokens with a comma, space (as they were inputted that way)
-					while (lineScan.hasNext()) {										//loop while there are still tokens to be read
-						counter++;														//update counter (1 more input)
-						double element = Double.parseDouble(lineScan.next());
-						inputs.add(element);				//parse the token to be a double and add to the input arraylist
-					}														//update counter to reflect input size (total - 1, since last token is output
-					counter--;
-					double[] passIn = new double[counter];								//this is the array that will be passed to sample class
-					for (int i = 0; i < counter; i++) {
-						passIn[i] = inputs.get(i);										//initialize the input array
-					}
-					samples.add(new Sample(passIn, inputs.get(counter)));				//create new sample with input array, output, and add that sample to the list of samples
-					numInputs = passIn.length;
-					numDataPoints ++;
-					lineScan.close();
-				}
-				s.close();
-				System.out.println("Number of Inputs: " + numInputs);
-				System.out.println("Number of Data Points: " + numDataPoints);
-			}
-			catch (Exception e) {
-				System.out.println("File not found.");
-			}
+		catch (Exception e) {
+			System.out.println("File not found.");
 		}
 
 		int numHidLayers = 2;
 		int numHidNodes = 10;
 		int numOutputs = 1;
 		int hiddenActivation = 2; //sigmoidal
-		int outputActivation = 1; //linear for function approximation
-
-		in.close();		
+		int outputActivation = 1; //linear for function approximation	
 			
 		//create network and train with backprop
-		network = new Network(numInputs, numHidLayers, numHidNodes, numOutputs, hiddenActivation, outputActivation);
+		Network network = new Network(numInputs, numHidLayers, numHidNodes, numOutputs, hiddenActivation, outputActivation);
 		trainWithBackprop(network, samples);
 		
 		//create initial population of networks and train with genetic algorithm
@@ -166,32 +110,26 @@ public class Main {
 	//train a population of networks with a genetic algorithm and evaluate
 	public static void trainWithGA(ArrayList<Network> population, ArrayList<Sample> samples) {
 		int genNum = 1;
-		for(int i = 0; i < 500; i++){	//a new generation is created every iteration
+		for(int i = 0; i < 100; i++){	//a new generation is created every iteration
 			double averageFitness = 0;
 			double bestFitness = Double.MAX_VALUE;
-			for(Network network : population) {
+			for(Network network : population) {	//iterate through population of networks
 				double fitness = network.evaluate(samples.subList(0, samples.size()/2));
 				network.setFitness(fitness);	//set the fitness of each to the average error of the test set
-				averageFitness += fitness;
-				if(fitness < bestFitness)
+				averageFitness += fitness;		//add together each fitness of population
+				if(fitness < bestFitness)	//find best fit individual - lowest average error
 					bestFitness = fitness;
 			}
-			averageFitness /= population.size();
+			averageFitness /= population.size();	//calculate average fitness of population
 			System.out.println("Best Fitness of Generation    " + (genNum) + ": " + bestFitness);
 			System.out.println("Average Fitness of Generation " + (genNum) + ": " + averageFitness + "\n");
 			genNum++;
-			/*for(Network network : population){
-				System.out.println("\t"+ network.getFitness());
-			}*/
 			System.out.println();
-			Collections.sort(population);	//sort networks by fitness
-			/*for(Network network : population){
-				System.out.println("\t"+ network.getFitness());
-			}*/
-			for(int j = 0; j < population.size(); j++)	//give worst network a fitness of 1, the next worse a fitness of 2, and so on
-				population.get(j).setFitness(j+1);
+			Collections.sort(population);	//sort networks by fitness because network implements comparable based on fitness value
+			for(int j = 0; j < population.size(); j++)	//give worst network a fitness of 1, the next a fitness of 2, and so on
+				population.get(j).setFitness(j+1);		//the best individual ends up with the highest fitness
 			
-			population = GeneticAlgorithm.createNextGeneration(population);
+			population = GeneticAlgorithm.createNextGeneration(population);	//create a new population of offspring
 		}
 	}
 }
