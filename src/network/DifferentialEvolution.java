f462800435a17cd557a035dbb972d6a44166ced9
Bryan Plant <bryanplant@gmail.com>
2017-12-01 16:49:44 -0700

    Revert "Main setup"

---
+++ src/network/DifferentialEvolution.java
@@ -0,0 +1,107 @@
+package network;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+/*	DifferentialEvolution class contains the necessary functions for the algorithm in relation to the population of networks.
+ */
+
+public class DifferentialEvolution {
+	private static Random rand = new Random();
+	private static double beta = 0.5; 				//scaling factor -- tunable
+	
+	//create the next generation of networks
+	//@param population - list of networks representing previous population
+	//@param sampes - list of samples/dataset for the algorithm
+	public static ArrayList<Network> createNextGeneration(ArrayList<Network> population, List<Sample> samples){
+		ArrayList<Network> newGeneration = new ArrayList<Network>(population);
+		newGeneration = crossover(newGeneration, samples);								//just crossover parents - no mutation
+		return newGeneration;
+	}
+	
+	//crossover parents into new generation
+	//@param parents - list of networks representing the parents, or previous generation
+	//@param samples - list of samples/dataset for the algorithm
+	public static ArrayList<Network> crossover(ArrayList<Network> parents, List<Sample> samples){
+		ArrayList<Network> offspring = new ArrayList<Network>();
+		
+		for(int num = 0; num < parents.size(); num++){									//each iteration a new parent will be selected
+			Network parent = parents.get(num);											//select parent to change
+			Network x1, x2, x3;
+			do {
+				x1 = parents.get(rand.nextInt(parents.size()));							//select x1
+			} while(x1 == parent);
+			do {
+				x2 = parents.get(rand.nextInt(parents.size()));							//select x2
+			} while(x2 == parent || x2 == x1);
+			do {
+				x3 = parents.get(rand.nextInt(parents.size()));							//select x3
+			} while(x3 == parent || x3 == x1 || x3 == x2);
+			ArrayList<ArrayList<Double>> u = createTrialVector(x1, x2, x3);				//create trial vector with x1, x2, x3
+			
+			ArrayList<ArrayList<Double>> parentGenes = parent.getGenes();				//get parent genes
+		    ArrayList<ArrayList<Double>> offspringGenes = new ArrayList<>();			//initialize offspring genes
+
+		    for(int i = 0; i < parentGenes.size(); i++){								//initialize the size of the outer offspring genes list and sigmas list
+		    	offspringGenes.add(new ArrayList<Double>());
+		    }
+		    
+		    for (int i = 0; i < parentGenes.size(); i++) {								//iterate through outer list
+		    	for(int j = 0; j < parentGenes.get(i).size(); j++) {					//iterate through inner list
+		    		if(rand.nextInt(2) == 0){											//select either 0 or 1 randomly
+		    			offspringGenes.get(i).add(parentGenes.get(i).get(j)); 			//set gene i of offspring 1 to gene i of parent
+		    		}
+		    		else{
+		    			offspringGenes.get(i).add(u.get(i).get(j)); 					//set gene i of offspring 1 to gene i of trial vector
+		    		}
+		    	}
+		    }
+		    //set the structure of offspring network the same as parent network
+		    int numInputs = parent.getNumInputs();
+		    int numHidLayers = parent.getNumHidLayers();
+		    int numHidNodes = parent.getNumHidNodes();
+		    int numOutputs = parent.getNumOutputs();
+		    int actFunHidden = parent.getActFunHidden();
+		    int actFunOutput = parent.getActFunOutput();
+		    
+		    //create network from offspring genes
+		    Network newNetwork = new Network(offspringGenes, numInputs, numHidLayers, numHidNodes, numOutputs, actFunHidden, actFunOutput);
+		    
+		    if(parent.getFitness() < newNetwork.evaluate(samples)) { 					//compare fitness of parent and offspring -- lower is better
+		    	offspring.add(parent);													//add parent if it has lower fitness than parent
+		    }
+		    else {
+		    	offspring.add(newNetwork);												//add child if it has a lower fitness than parent
+		    }
+		}
+		return offspring;																//return population of offspring
+	}
+	
+	//creates the "trail vector" required of DE
+	//@param x1 - first parent vector
+	//@param x2 - second parent vector
+	//@param x3 - third parent vector
+	public static ArrayList<ArrayList<Double>> createTrialVector(Network x1, Network x2, Network x3) {	
+		ArrayList<ArrayList<Double>> diff1 = x2.getGenes(); 							//get genes of difference vector 1
+		ArrayList<ArrayList<Double>> diff2 = x3.getGenes(); 							//get genes of difference vector 2
+		ArrayList<ArrayList<Double>> diff = new ArrayList<>(); 							//genes of result of difference vectors
+		
+		for(int i = 0; i < diff1.size(); i++) {
+			diff.add(new ArrayList<Double>());
+			for(int j = 0; j < diff1.get(i).size(); j++) {
+				diff.get(i).add(beta*(diff1.get(i).get(j) - diff2.get(i).get(j))); 		//calculate difference of genes and multiply by beta
+			}
+		}
+		
+		ArrayList<ArrayList<Double>> target = x1.getGenes(); 							//set target vector to the genes of x1
+		ArrayList<ArrayList<Double>> u = new ArrayList<>(); 							//genes of resulting trial vector
+		for(int i = 0; i < target.size(); i++) {
+			u.add(new ArrayList<Double>());
+			for(int j = 0; j < target.get(i).size(); j++) {
+				u.get(i).add(target.get(i).get(j) + diff.get(i).get(j));
+			}
+		}
+		
+		return u;																		//return trial vector
+	}
+}
--- src/network/Network.java
+++ src/network/Network.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * 	is changed a bit from Project 2, as it had to incorporate new functionality for the 3 new training algorithms.
  */
 
-public class CNN implements Comparable<CNN>{
+public class Network implements Comparable<Network>{
 	private Random random = new Random();
 	private ArrayList<Layer> layers;
 	private double learningRate;
@@ -31,7 +31,7 @@ public class CNN implements Comparable<CNN>{
 	 * @param numOutputs: number of output nodes
 	 * @param actFun: type of activation function for nodes
 	 */
-	public CNN(int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFunHidden, int actFunOutput) {
+	public Network(int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFunHidden, int actFunOutput) {
 		layers = new ArrayList<Layer>();
 		//create input layer with inputs number of nodes and a linear activation function
 		layers.add(new Layer(numInputs, 1));
@@ -89,7 +89,7 @@ public class CNN implements Comparable<CNN>{
 	//@param numOutputs - number of outputs the network has
 	//@param actFunHidden - activation function for the hidden layers
 	//@param actFunOutput - activation function for the output layer
-	public CNN(ArrayList<ArrayList<Double>> genes, int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFunHidden, int actFunOutput) {
+	public Network(ArrayList<ArrayList<Double>> genes, int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFunHidden, int actFunOutput) {
 		layers = new ArrayList<Layer>();
 		//create input layer with inputs number of nodes and a linear activation function
 		layers.add(new Layer(numInputs, 1));
@@ -133,7 +133,7 @@ public class CNN implements Comparable<CNN>{
 	//@param numOutputs - number of outputs the network has
 	//@param actFunHidden - activation function for the hidden layers
 	//@param actFunOutput - activation function for the output layer
-	public CNN(ArrayList<ArrayList<Double>> genes, ArrayList<ArrayList<Double>> sigmas, int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFunHidden, int actFunOutput) {
+	public Network(ArrayList<ArrayList<Double>> genes, ArrayList<ArrayList<Double>> sigmas, int numInputs, int numHidLayers, int numHidNodes, int numOutputs, int actFunHidden, int actFunOutput) {
 		layers = new ArrayList<Layer>();
 		//create input layer with inputs number of nodes and a linear activation function
 		layers.add(new Layer(numInputs, 1));
@@ -167,12 +167,6 @@ public class CNN implements Comparable<CNN>{
 		this.numOutputs = numOutputs;
 		this.actFunHidden = actFunHidden;
 		this.actFunOutput = actFunOutput;
-	}
-	
-
-	public ArrayList<Cluster> cluster(ArrayList<DataPoint> data) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	//Randomly reset weights in network
@@ -260,17 +254,17 @@ public class CNN implements Comparable<CNN>{
 		return error;	//return absolute error
 	}
 
-/*	//evaluates the newtwork's fitness by calculating the error
+	//evaluates the newtwork's fitness by calculating the error
 	//@param samples - a list of samples, or the dataset
-	public double evaluate(List<DataPoint> samples){	
+	public double evaluate(List<Sample> samples){	
 		double error = 0;
-		for(DataPoint sample : samples){
+		for(Sample sample : samples){
 			calcOutputs(sample.getInputs());
 			double actualOutput = layers.get(layers.size()-1).getNeuron(0).getOutput();
-			error += Math.abs(actualOutput - sample.getClassVal());
+			error += Math.abs(actualOutput - sample.getOutput());
 		}	
 		return error/samples.size();	//return average error
-	}*/
+	}
 
 	//prints out information about network
 	public void printNetwork(){
@@ -369,7 +363,7 @@ public class CNN implements Comparable<CNN>{
 
 	//compares the fitness of two networks
 	@Override
-	public int compareTo(CNN o) {
+	public int compareTo(Network o) {
 		if(this.fitness < o.fitness)
 			return 1;
 		return -1;
